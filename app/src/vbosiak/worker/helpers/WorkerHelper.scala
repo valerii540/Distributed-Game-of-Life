9e15dbbdd28bec2ab6d91b8d7eeedf9bf5f030e8
valerii540 <valerii540@protonmail.com>
2021-11-26 18:17:33 +0200

    Code clean up

--- app/src/vbosiak/worker/helpers/WorkerHelper.scala
+++ app/src/vbosiak/worker/helpers/WorkerHelper.scala
@@ -53,29 +53,14 @@ private[worker] trait WorkerHelper {
       standAlone: Boolean = false,
       inParallel: Boolean = true
   ): (Field, Int, FiniteDuration) = {
-//    def computeRow(row: ArraySeq[Boolean], r: Int, population: AtomicInteger, copy: ArraySeq[Array[Boolean]]): Unit =
-//      for (c <- row.indices) {
-//        val isAlive = row(c)
-//
-//        val aliveNeighbors: Int = Seq(
-//          checkCell(field, r - 1, c, standAlone),                // top
-//          checkCell(field, r + 1, c, standAlone),                // bottom
-//          checkCell(field, r, c + 1, standAlone, rightSide),     // right
-//          checkCell(field, r, c - 1, standAlone, leftSide),      // left
-//          checkCell(field, r - 1, c + 1, standAlone, rightSide), // top-right
-//          checkCell(field, r - 1, c - 1, standAlone, leftSide),  // top-left
-//          checkCell(field, r + 1, c + 1, standAlone, rightSide), // bottom-right
-//          checkCell(field, r + 1, c - 1, standAlone, leftSide)   // bottom-left
-//        ).count(identity)
-//
-//        if (isAlive && (aliveNeighbors == 2 || aliveNeighbors == 3))
-//          population.incrementAndGet()
-//        else if (!isAlive && aliveNeighbors == 3) {
-//          copy(r)(c) = true
-//          population.incrementAndGet()
-//        } else
-//          copy(r)(c) = false
-//      }
+    def mutate(isAlive: Boolean, population: AtomicInteger, copy: ArraySeq[Array[Boolean]], aliveNeighbors: Int, r: Int, c: Int): Unit =
+      if (isAlive && (aliveNeighbors == 2 || aliveNeighbors == 3))
+        population.incrementAndGet()
+      else if (!isAlive && aliveNeighbors == 3) {
+        copy(r)(c) = true
+        population.incrementAndGet()
+      } else
+        copy(r)(c) = false
 
     def computeInnerTop(population: AtomicInteger, copy: ArraySeq[Array[Boolean]]): Unit =
       (1 until field.head.size - 1).foreach { c =>
@@ -103,110 +88,90 @@ private[worker] trait WorkerHelper {
 
     def computeInnerBottom(population: AtomicInteger, copy: ArraySeq[Array[Boolean]]): Unit =
       (1 until field.head.size - 1).foreach { c =>
-        val isAlive  = field.last(c)
-        val lastRIdx = field.size - 1
+        val lastR = field.size - 1
 
-        val aliveNeighbors: Int = Seq(
-          field(lastRIdx - 1)(c),     // top
-          field(0)(c),                // bottom
-          field(lastRIdx)(c + 1),     // right
-          field(lastRIdx)(c - 1),     // left
-          field(lastRIdx - 1)(c + 1), // top-right
-          field(lastRIdx - 1)(c - 1), // top-left
-          field(0)(c + 1),            // bottom-right
-          field(0)(c - 1)             // bottom-left
-        ).count(identity)
-
-        if (isAlive && (aliveNeighbors == 2 || aliveNeighbors == 3))
-          population.incrementAndGet()
-        else if (!isAlive && aliveNeighbors == 3) {
-          copy(lastRIdx)(c) = true
-          population.incrementAndGet()
-        } else
-          copy(lastRIdx)(c) = false
+        mutate(
+          field.last(c),
+          population,
+          copy,
+          Seq(
+            field(lastR - 1)(c),     // top
+            field(0)(c),             // bottom
+            field(lastR)(c + 1),     // right
+            field(lastR)(c - 1),     // left
+            field(lastR - 1)(c + 1), // top-right
+            field(lastR - 1)(c - 1), // top-left
+            field(0)(c + 1),         // bottom-right
+            field(0)(c - 1)          // bottom-left
+          ).count(identity),
+          lastR,
+          c
+        )
       }
 
     def computeInnerRow(row: ArraySeq[Boolean], r: Int, population: AtomicInteger, copy: ArraySeq[Array[Boolean]]): Unit =
       (1 until row.size - 1).foreach { c =>
-        val isAlive = row(c)
-
-        val aliveNeighbors: Int = Seq(
-          field(r - 1)(c),     // top
-          field(r + 1)(c),     // bottom
-          field(r)(c + 1),     // right
-          field(r)(c - 1),     // left
-          field(r - 1)(c + 1), // top-right
-          field(r - 1)(c - 1), // top-left
-          field(r + 1)(c + 1), // bottom-right
-          field(r + 1)(c - 1)  // bottom-left
-        ).count(identity)
-
-        if (isAlive && (aliveNeighbors == 2 || aliveNeighbors == 3))
-          population.incrementAndGet()
-        else if (!isAlive && aliveNeighbors == 3) {
-          copy(r)(c) = true
-          population.incrementAndGet()
-        } else
-          copy(r)(c) = false
+        mutate(
+          isAlive = row(c),
+          population = population,
+          copy = copy,
+          aliveNeighbors = Seq(
+            field(r - 1)(c),     // top
+            field(r + 1)(c),     // bottom
+            field(r)(c + 1),     // right
+            field(r)(c - 1),     // left
+            field(r - 1)(c + 1), // top-right
+            field(r - 1)(c - 1), // top-left
+            field(r + 1)(c + 1), // bottom-right
+            field(r + 1)(c - 1)  // bottom-left
+          ).count(identity),
+          r = r,
+          c = c
+        )
       }
 
-    def computeLeftCell(r: Int, population: AtomicInteger, copy: ArraySeq[Array[Boolean]]): Unit = {
-      val isAlive = field(r).head
-
-      val aliveNeighbors: Int = Seq(
-        field(r - 1).head,                                                    // top
-        field(r + 1).head,                                                    // bottom
-        field(r)(1),                                                          // right
-        if (standAlone) field(r)(field(r).size - 1) else leftSide(r),         // left
-        field(r - 1)(1),                                                      // top-right
-        if (standAlone) field(r - 1)(field(r).size - 1) else leftSide(r - 1), // top-left
-        field(r + 1)(1),                                                      // bottom-right
-        if (standAlone) field(r + 1)(field(r).size - 1) else leftSide(r + 1)  // bottom-left
-      ).count(identity)
-
-      if (isAlive && (aliveNeighbors == 2 || aliveNeighbors == 3))
-        population.incrementAndGet()
-      else if (!isAlive && aliveNeighbors == 3) {
-        copy(r)(0) = true
-        population.incrementAndGet()
-      } else
-        copy(r)(0) = false
-    }
+    def computeLeftCell(r: Int, population: AtomicInteger, copy: ArraySeq[Array[Boolean]]): Unit =
+      mutate(
+        field(r).head,
+        population,
+        copy,
+        Seq(
+          field(r - 1).head,                                                    // top
+          field(r + 1).head,                                                    // bottom
+          field(r)(1),                                                          // right
+          if (standAlone) field(r)(field(r).size - 1) else leftSide(r),         // left
+          field(r - 1)(1),                                                      // top-right
+          if (standAlone) field(r - 1)(field(r).size - 1) else leftSide(r - 1), // top-left
+          field(r + 1)(1),                                                      // bottom-right
+          if (standAlone) field(r + 1)(field(r).size - 1) else leftSide(r + 1)  // bottom-left
+        ).count(identity),
+        r,
+        0
+      )
 
     def computeRightCell(r: Int, population: AtomicInteger, copy: ArraySeq[Array[Boolean]]): Unit = {
-      val isAlive = field(r).last
       val lastIdx = field(r).size - 1
 
-      val aliveNeighbors: Int = Seq(
-        field(r - 1).last,                                     // top
-        field(r + 1).last,                                     // bottom
-        if (standAlone) field(r)(0) else rightSide(r),         // right
-        field(r)(lastIdx - 1),                                 // left
-        if (standAlone) field(r - 1)(0) else rightSide(r - 1), // top-right
-        field(r - 1)(lastIdx - 1),                             // top-left
-        if (standAlone) field(r + 1)(0) else rightSide(r + 1), // bottom-right
-        field(r + 1)(lastIdx - 1)                              // bottom-left
-      ).count(identity)
-
-      if (isAlive && (aliveNeighbors == 2 || aliveNeighbors == 3))
-        population.incrementAndGet()
-      else if (!isAlive && aliveNeighbors == 3) {
-        copy(r)(lastIdx) = true
-        population.incrementAndGet()
-      } else
-        copy(r)(lastIdx) = false
+      mutate(
+        field(r).last,
+        population,
+        copy,
+        Seq(
+          field(r - 1).last,                                     // top
+          field(r + 1).last,                                     // bottom
+          if (standAlone) field(r)(0) else rightSide(r),         // right
+          field(r)(lastIdx - 1),                                 // left
+          if (standAlone) field(r - 1)(0) else rightSide(r - 1), // top-right
+          field(r - 1)(lastIdx - 1),                             // top-left
+          if (standAlone) field(r + 1)(0) else rightSide(r + 1), // bottom-right
+          field(r + 1)(lastIdx - 1)                              // bottom-left
+        ).count(identity),
+        r,
+        lastIdx
+      )
     }
 
     def computeCorners(population: AtomicInteger, copy: ArraySeq[Array[Boolean]]): Unit = {
-      def mutate(isAlive: Boolean, aliveNeighbors: Int, r: Int, c: Int): Unit =
-        if (isAlive && (aliveNeighbors == 2 || aliveNeighbors == 3))
-          population.incrementAndGet()
-        else if (!isAlive && aliveNeighbors == 3) {
-          copy(r)(c) = true
-          population.incrementAndGet()
-        } else
-          copy(r)(c) = false
-
       val topR    = 0
       val bottomR = field.size - 1
 
@@ -214,10 +179,11 @@ private[worker] trait WorkerHelper {
       val rightC = field.head.size - 1
 
       // TOP LEFT
-      {
-        val isAlive = field(topR)(leftC)
-
-        val aliveNeighbors: Int = Seq(
+      mutate(
+        field(topR)(leftC),
+        population,
+        copy,
+        Seq(
           field(bottomR)(leftC),                                          // top
           field(topR + 1)(leftC),                                         // bottom
           field(topR)(leftC + 1),                                         // right
@@ -226,16 +192,17 @@ private[worker] trait WorkerHelper {
           if (standAlone) field(bottomR)(rightC) else leftSide(bottomR),  // top-left
           field(topR + 1)(leftC + 1),                                     // bottom-right
           if (standAlone) field(topR + 1)(rightC) else leftSide(topR + 1) // bottom-left
-        ).count(identity)
-
-        mutate(isAlive, aliveNeighbors, topR, leftC)
-      }
+        ).count(identity),
+        topR,
+        leftC
+      )
 
       // TOP RIGHT
-      {
-        val isAlive = field(topR)(rightC)
-
-        val aliveNeighbors: Int = Seq(
+      mutate(
+        field(topR)(rightC),
+        population,
+        copy,
+        Seq(
           field(bottomR)(rightC),                                          // top
           field(1)(rightC),                                                // bottom
           if (standAlone) field(topR)(leftC) else rightSide(topR),         // right
@@ -244,16 +211,17 @@ private[worker] trait WorkerHelper {
           field(bottomR)(rightC - 1),                                      // top-left
           if (standAlone) field(topR + 1)(leftC) else rightSide(topR + 1), // bottom-right
           field(topR + 1)(rightC - 1)                                      // bottom-left
-        ).count(identity)
-
-        mutate(isAlive, aliveNeighbors, topR, rightC)
-      }
+        ).count(identity),
+        topR,
+        rightC
+      )
 
       // BOTTOM LEFT
-      {
-        val isAlive = field(bottomR)(leftC)
-
-        val aliveNeighbors: Int = Seq(
+      mutate(
+        field(bottomR)(leftC),
+        population,
+        copy,
+        Seq(
           field(bottomR - 1)(leftC),                                             // top
           field(topR)(leftC),                                                    // bottom
           field(bottomR)(leftC + 1),                                             // right
@@ -262,16 +230,17 @@ private[worker] trait WorkerHelper {
           if (standAlone) field(bottomR - 1)(rightC) else leftSide(bottomR - 1), // top-left
           field(topR)(leftC + 1),                                                // bottom-right
           if (standAlone) field(topR)(rightC) else leftSide(topR)                // bottom-left
-        ).count(identity)
-
-        mutate(isAlive, aliveNeighbors, bottomR, leftC)
-      }
+        ).count(identity),
+        bottomR,
+        leftC
+      )
 
       // BOTTOM RIGHT
-      {
-        val isAlive = field(bottomR)(rightC)
-
-        val aliveNeighbors: Int = Seq(
+      mutate(
+        field(bottomR)(rightC),
+        population,
+        copy,
+        Seq(
           field(bottomR - 1)(rightC),                                            // top
           field(topR)(rightC),                                                   // bottom
           if (standAlone) field(bottomR)(leftC) else rightSide(bottomR),         // right
@@ -280,10 +249,10 @@ private[worker] trait WorkerHelper {
           field(bottomR - 1)(rightC - 1),                                        // top-left
           if (standAlone) field(topR)(leftC) else rightSide(topR),               // bottom-right
           field(topR)(rightC - 1)                                                // bottom-left
-        ).count(identity)
-
-        mutate(isAlive, aliveNeighbors, bottomR, rightC)
-      }
+        ).count(identity),
+        bottomR,
+        rightC
+      )
     }
 
     val ((finishedField, population), duration) = Clock.withMeasuring {
@@ -291,27 +260,22 @@ private[worker] trait WorkerHelper {
       val fieldCopy  = field.map(_.toArray)
       val population = new AtomicInteger(0)
 
-      if (inParallel) {
-        computeInnerTop(population, fieldCopy)
-        computeInnerBottom(population, fieldCopy)
-        computeCorners(population, fieldCopy)
+      computeInnerTop(population, fieldCopy)
+      computeInnerBottom(population, fieldCopy)
+      computeCorners(population, fieldCopy)
 
+      if (inParallel)
         (1 until field.size - 1).par.foreach { r =>
           computeLeftCell(r, population, fieldCopy)
           computeInnerRow(field(r), r, population, fieldCopy)
           computeRightCell(r, population, fieldCopy)
         }
-      } else {
-        computeInnerTop(population, fieldCopy)
-        computeInnerBottom(population, fieldCopy)
-        computeCorners(population, fieldCopy)
-
+      else
         (1 until field.size - 1).foreach { r =>
           computeLeftCell(r, population, fieldCopy)
           computeInnerRow(field(r), r, population, fieldCopy)
           computeRightCell(r, population, fieldCopy)
         }
-      }
 
       fieldCopy.map(_.to(ArraySeq)) -> population.get()
     }
